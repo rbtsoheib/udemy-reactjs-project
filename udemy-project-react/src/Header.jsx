f13ff315bc096de4cd04224cf7f3f4ecd8429dfa
rbtsoheib <taharsoheib08@gmail.com>
2024-08-17 01:50:36 +0200

    commit

---
+++ udemy-project-react/src/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+
+return (
+
+<>
+
+<h1 style={{color:'blue'}}> HELLO WORLD </h1>
+
+<h2> well in this lecture we will be talking about LOVBVVEEEVEVVEVEVLOVBVVEEEVEVVEVEV</h2>
+
+</>
+)
+
+}
+
+export default Header;
