67329af2b6b53f520c0c3ec8b612e42063444669
rbtsoheib <taharsoheib08@gmail.com>
2024-08-18 04:07:07 +0200

    commit jsx 2

--- udemy-project-react/src/Header.jsx
+++ udemy-project-react/src/Header.jsx
@@ -8,21 +8,22 @@ return (
 
 <div style={{background:'Blue' , width:'570px' , height:'370px'}}>
 
-<h1 style={{color:'green' , fontSize:'21px'}}> HELLO WORLD </h1>
+    <h1 style={{color:'green' , fontSize:'21px'}}> HELLO WORLD </h1>
 
-<h2 style={{color:'violet' , fontSize:'20px'}}> well in this lecture we will be talking about LOVBVVEEEVEVVEVEVLOVBVVEEEVEVVEVEV</h2>
+    <h2 style={{color:'violet' , fontSize:'21px'}}> well in this lecture we will be talking about LOVBVVEEEVEVVEVEVLOVBVVEEEVEVVEVEV </h2>
 
-<h3 style={{color:'chocolate' , fontSize:'25px'}}> this is a heading n°3 </h3>
+    <h3 style={{color:'chocolate' , fontSize:'21px'}}> this is a heading n°3 </h3>
 
-<h4 style={{color:'darkcyan' , fontSize:'21px'}}> this is a fourth heading </h4>
+    <h4 style={{color:'darkcyan' , fontSize:'21px'}}> this is a fourth heading </h4>
 
-<h5 style={{color:'deeppink' , fontSize:'23px'}}> this is a fifth heading </h5>
+    <h5 style={{color:'deeppink' , fontSize:'21px'}}> this is a fifth heading </h5>
 
-<h6 style={{color:'dimgray' , fontSize:'20px'}}> this is a sixth heading </h6>
+    <h6 style={{color:'dimgray' , fontSize:'21px'}}> this is a sixth heading </h6>
 
 </div>
 
 </>
+
 )
 
 }
