2b0745500f15517e4e7f0aded53724d968700ce9
rbtsoheib <taharsoheib08@gmail.com>
2024-08-18 03:51:06 +0200

    commit jsx Header

--- udemy-project-react/src/Header.jsx
+++ udemy-project-react/src/Header.jsx
@@ -6,9 +6,21 @@ return (
 
 <>
 
-<h1 style={{color:'blue'}}> HELLO WORLD </h1>
+<div style={{background:'Blue' , width:'570px' , height:'370px'}}>
 
-<h2> well in this lecture we will be talking about LOVBVVEEEVEVVEVEVLOVBVVEEEVEVVEVEV</h2>
+<h1 style={{color:'green' , fontSize:'21px'}}> HELLO WORLD </h1>
+
+<h2 style={{color:'violet' , fontSize:'20px'}}> well in this lecture we will be talking about LOVBVVEEEVEVVEVEVLOVBVVEEEVEVVEVEV</h2>
+
+<h3 style={{color:'chocolate' , fontSize:'25px'}}> this is a heading n°3 </h3>
+
+<h4 style={{color:'darkcyan' , fontSize:'21px'}}> this is a fourth heading </h4>
+
+<h5 style={{color:'deeppink' , fontSize:'23px'}}> this is a fifth heading </h5>
+
+<h6 style={{color:'dimgray' , fontSize:'20px'}}> this is a sixth heading </h6>
+
+</div>
 
 </>
 )
--- udemy-project-react/src/index.js
+++ udemy-project-react/src/index.js
@@ -14,5 +14,3 @@ document.getElementById('root'),
 
 );
 
-
-
