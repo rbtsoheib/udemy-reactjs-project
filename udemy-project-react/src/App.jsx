e63ae6deb638ed83192523acb960f6ece67b46e9
rbtsoheib <taharsoheib08@gmail.com>
2024-08-27 04:18:47 +0200

    last changes for 27/8/2024

--- udemy-project-react/src/App.jsx
+++ udemy-project-react/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Counter from "./Components/Counter";
 import Header from "./Components/Header";
 import "./App.css";
--- udemy-project-react/src/Components/Counter.jsx
+++ udemy-project-react/src/Components/Counter.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import { useState } from "react";
 import "./Counter.css";
 
 function Counter() {
+  let count = 0;
+  const [currentstate, updatestate] = useState(count);
+  const handleclick = () => {
+    updatestate(currentstate + 1);
+  };
+
+  let number = 1; 
+  const [firststate,newstate] = useState(number); 
+  const clickme = () => {
+
+    newstate(firststate + 1 );
+
+  };
+
   return (
     <>
       <div className="Counter_parent">
         <div className="main-div">
           <h1> this is a Counter </h1>
-          <button>0</button>
+          <button onClick={handleclick}>{currentstate}</button>
+          <button onClick={clickme}> {firststate} </button>
         </div>
       </div>
     </>
